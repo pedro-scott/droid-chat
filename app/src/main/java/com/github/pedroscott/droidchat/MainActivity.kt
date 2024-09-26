1df5f649877286ffddaba2846d8b62d5a0670322
Pedro Scott <pedroscott.dev@gmail.com>
2024-09-26 12:22:24 -0300

    feat: Update project colors

--- app/src/main/java/com/github/pedroscott/droidchat/MainActivity.kt
+++ app/src/main/java/com/github/pedroscott/droidchat/MainActivity.kt
@@ -3,7 +3,6 @@ package com.github.pedroscott.droidchat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,7 +15,6 @@ import com.github.pedroscott.droidchat.ui.theme.DroidChatTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DroidChatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
